06c6aec9226062ef3925dd50ba232d4235ff8e23
Dave-Njeru <davenjeru9@gmail.com>
2025-05-13 22:59:05 +0300

    fix: fetch url

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch"; // import custom hook
 
 const Home = () => {
-    const {data: blogs, isLoading, error} = useFetch('https://my-json-server.typicode.com/Dave-Njeru/dojo-blogs');
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="name">
